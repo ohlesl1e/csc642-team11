a0504a8c4ff322d94b623cd4fd03673750646217
ohlesl1e <ohlesl1e420@gmail.com>
2021-12-03 16:13:04 -0800

    renamed some files

--- source/src/App.js
+++ source/src/App.js
@@ -6,8 +6,8 @@ import Header from './pages/Header';
 import Home from './pages/Home';
 
 
-import Seller from './pages/Seller Page';
-import Listing from './pages/Create Listing';
+import Seller from './pages/SellerPage';
+import Listing from './pages/CreateListing';
 import Search from './pages/Search';
 import Result from './pages/Result';
 import Registration from './pages/Registration';
@@ -17,7 +17,7 @@ import Confirmation from './pages/Confirmation';
 
 function App() {
     return (
-        <div className="App" style={{height: '100vh'}}>
+        <div className="App" style={{ height: '100vh' }}>
             <Header />
             <Routes>
                 <Route exact path='/' element={<Home />} />
@@ -27,6 +27,10 @@ function App() {
                 <Route path='/Login' element={<Login />} />
                 <Route path='/checkout' element={<Checkout />} />
                 <Route path='/confirmation' element={<Confirmation />} />
+                <Route path='/notadmin' element={<Login />} />
+                <Route path='/seller' element={<Seller />} />
+                <Route path='/listing' element={<Listing />} />
+
             </Routes>
             <Footer />
         </div>
